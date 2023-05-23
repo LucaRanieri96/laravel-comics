ba0803a7e9852abefc1097b9bca3e9db642594f3
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-23 16:26:36 +0200

    Add Home content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,26 @@
 @extends('layout.app')
 
 @section('content')
-    <p>CIAO</p>
+<div id="banner"></div>
+
+<div class="main_top">
+  <div class="container">
+    <div id="title">
+      <h3>CURRENT SERIES</h3>
+    </div>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6 py-5">
+        @foreach ($comics as $comic)
+        <div class="col">
+            <div>
+                <img src="{{$comic["thumb"]}}" alt="type">
+                <p class="mt-2">{{$comic["title"]}}</p>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    <div class="d-flex justify-content-center pb-4">
+      <button type="button" class="btn btn-primary">Load More</button>
+    </div>
+  </div>
+</div>
 @endsection
