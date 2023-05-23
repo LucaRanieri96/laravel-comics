a7d90ad3aecd7281414b43192e9bbc39b9486f43
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-23 15:49:48 +0200

    Add header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+
+<div class="blue">
+    <div class="container d-flex justify-content-end">
+        <span>DC POWER VISA</span>
+        <span class="ms-5">ADDITIONAL DC SITES</span>
+    </div>
+</div>
+<header id="app_header" class="py-2">
+    <div class="container d-flex align-items-center justify-content-between">
+      <div class="logo">
+        <img src="/assets/images/dc-logo.png" alt="" />
+      </div>
+      <div class="nav">
+        <div><a href="#">COMICS</a></div>
+        <div><a href="#">MOVIES</a></div>
+        <div><a href="#">TV</a></div>
+        <div><a href="#">GAMES</a></div>
+        <div><a href="#">COLLECTIBLES</a></div>
+        <div><a href="#">VIDEOS</a></div>
+        <div><a href="#">FANS</a></div>
+        <div><a href="#">NEWS</a></div>
+        <div><a href="#">SHOP</a></div>
+      </div>
+      <div>
+          <input type="text" placeholder="Search" class="input">
+      </div>
+    </div>
+  </header>
